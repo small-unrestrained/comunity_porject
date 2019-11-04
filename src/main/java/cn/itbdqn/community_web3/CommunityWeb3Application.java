1e415b7847f7ba441c26f039368757ca7a11c50f
small-unrestrained <17723624551@163.com>
2019-11-04 21:52:44 +0800

    init repo

---
+++ src/main/java/cn/itbdqn/community_web3/CommunityWeb3Application.java
@@ -0,0 +1,13 @@
+package cn.itbdqn.community_web3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityWeb3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityWeb3Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/itbdqn/community_web3/CommunityWeb3ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.itbdqn.community_web3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityWeb3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
